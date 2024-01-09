5b115cb886b0692c1ea04774d27664a470d342a0
jereml99 <ledwojeremi@gmail.com>
2024-01-09 12:21:50 +0100

    Add SportDataModule and test cases

---
+++ tests/data/test_DataModule.py
@@ -0,0 +1,30 @@
+import pytest
+from src.data.DataModule import SportDataModule
+
+@pytest.fixture
+def datamodule():
+    return SportDataModule(data_dir='./data', batch_size=32, num_workers=4, transformations=None, train_val_test_split=[0.7, 0.15, 0.15])
+
+def test_setup(datamodule):
+    datamodule.setup()
+    assert datamodule.train_dataset is not None
+    assert datamodule.val_dataset is not None
+    assert datamodule.test_dataset is not None
+
+def test_train_dataloader(datamodule):
+    datamodule.setup()
+    train_dataloader = datamodule.train_dataloader()
+    assert len(train_dataloader.dataset) > 0
+    assert len(train_dataloader) > 0
+
+def test_val_dataloader(datamodule):
+    datamodule.setup()
+    val_dataloader = datamodule.val_dataloader()
+    assert len(val_dataloader.dataset) > 0
+    assert len(val_dataloader) > 0
+
+def test_test_dataloader(datamodule):
+    datamodule.setup()
+    test_dataloader = datamodule.test_dataloader()
+    assert len(test_dataloader.dataset) > 0
+    assert len(test_dataloader) > 0
