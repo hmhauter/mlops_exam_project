85de3ca187fe51f620d0e00804cfc40c4049cc4b
helena <helenam.hauter@gmail.com>
2024-01-11 12:09:59 +0100

    add test LightningCLI

--- tests/test_LightningCLI.py
+++ tests/test_LightningCLI.py
@@ -1,15 +1,18 @@
+from pathlib import Path
 from src.main import cli_main
 import pytest
 from unittest.mock import patch
 from io import StringIO
 import sys
 
-@patch("sys.argv", ["main.py", "-c", "config/main.yaml", "fit"])
+@patch("sys.argv", ["main.py", "-c", "config/test.yaml", "fit"])
 def test_cli_main():
+    print("!!!!!!!!!!!!!!!")
     captured_output = StringIO()
     sys.stdout = captured_output
     cli_main()
     sys.stdout = sys.__stdout__
-    assert("Expected Output", captured_output.getvalue())
+    print(captured_output)
+    assert Path("config.yaml").exists()
 
 
