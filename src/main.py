c0ae632d504442fb4f0acd7707c1e263bf1c3a0a
Jeremi Ledwoń <32729855+jereml99@users.noreply.github.com>
2024-01-12 14:43:44 +0100

    Fix configuration logging (#15)
    * Fix configuration logging
    
    * Revert "Fix configuration logging"
    
    This reverts commit 292226463a69af675e2e5f2d141899f94cb09970.
    
    * Fix wandb to log config

--- src/main.py
+++ src/main.py
@@ -3,11 +3,18 @@ import logging
 
 from src.data.DataModule import SportDataModule
 from src.models.model import CustomModel
+from lightning.pytorch.cli import SaveConfigCallback
+from lightning.pytorch.loggers import Logger
 
+
+class CustomSaveConfigCallback(SaveConfigCallback):
+    def save_config(self, trainer, pl_module, stage):
+        if isinstance(trainer.logger, Logger):
+            config = self.parser.dump(self.config, skip_none=False)  # Required for proper reproducibility
+            trainer.logger.log_hyperparams({"config": config})
 
 def cli_main():
-    LightningCLI(CustomModel, SportDataModule)
-
+    LightningCLI(CustomModel, SportDataModule, save_config_callback=CustomSaveConfigCallback, save_config_kwargs={"save_to_log_dir": False})
 
 if __name__ == "__main__":
     logging.Logger("lightning", level=logging.INFO)
