21a826c418d737666ccb0073cbe73fc0fe95c9cf
helena <helenam.hauter@gmail.com>
2024-01-14 13:00:33 +0100

    delete debug statements

--- src/prediction_server.py
+++ src/prediction_server.py
@@ -5,6 +5,7 @@ from PIL import Image
 import torch
 from google.cloud import storage
 from src.models.model import CustomModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -12,24 +13,26 @@ app = FastAPI()
 BUCKET_NAME = "test-model-server"
 MODEL_FILE_NAME = "model.ckpt"
 
-# service_account_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON")
-# client = storage.Client.from_service_account_info(service_account_json)
+origins = ["*"]
 
-# credentials = service_account.Credentials.from_service_account_info(service_account_json)
-# client = storage.Client(project='project_id', credentials=credentials)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 client = storage.Client("mlops-exam-project")
 # Download the model from GCS using the bucket and blob
-print("DOWNLOAD MODEL")
 bucket = client.get_bucket(BUCKET_NAME)
 blob = bucket.get_blob(MODEL_FILE_NAME)
 model_bytes = blob.download_as_bytes()
-print("GOT MODEL BYTES")
+
 # Load the model using torch.load
 model = CustomModel.load_from_checkpoint(io.BytesIO(model_bytes))
 model.freeze()
 model.to('cuda' if torch.cuda.is_available() else 'cpu')  # Move model to GPU if available
-print("YES - GOT THE MODEL")
 
 # Hard coded classes - what would be the best way to do this?
 idx_to_class = {0: 'Badminton', 1: 'Cricket', 2: 'Tennis', 3: 'Swimming', 4: 'Soccer', 5: 'Wrestling', 6: 'Karate'}
